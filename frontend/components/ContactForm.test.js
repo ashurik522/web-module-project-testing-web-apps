9535313e8a4628b353130cb69cb2b0fe5a725d43
ashurik522 <ashurik522@gmail.com>
2022-06-09 15:09:19 -0400

    through test 6

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -43,11 +43,28 @@ test('renders THREE error messages if user enters no values into any fields.', a
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
     render(<ContactForm />)
 
+    const firstNameInput = screen.getByLabelText(/first name*/i)
+    const lastNameInput = screen.getByLabelText(/last name*/i)
+    const button = screen.getByRole(/button/, { name: /Submit/i })
+    
+    userEvent.type(firstNameInput, 'Blade')
+    userEvent.type(lastNameInput, 'Thomas')
+    userEvent.click(button)
+
+    const errorMessages = await screen.findAllByTestId(/error/i)
+    expect(errorMessages).toHaveLength(1)
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
     render(<ContactForm />)
 
+    const emailInput = screen.getByLabelText(/email*/i)
+
+    userEvent.type(emailInput, 'user@user')
+
+    const errorMessage = await screen.findByText(/email must be a valid email address/i)
+
+    expect(errorMessage).toBeInTheDocument()
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
